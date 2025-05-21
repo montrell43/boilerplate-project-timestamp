8d0d185e07fbd0b574064f08128aad856c688237
montrell43 <tmerriweather@persevernow.org>
2025-05-21 15:10:03 -0500

    another commit

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.get("/api/whoami", function (req, res) {
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
